96ae1807f76d1b751b35637b0545f3cd15cc7f37
denz1994 <denz1994@gmail.com>
2023-01-22 02:17:01 -0500

    Change port to 3001

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.get('/users/usersPerCountry/:country', async (req, res) => {
     }
 });
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
     console.log(`Server listening on ${port}`);
 })
