be631945df30922586698daffe4f8e8e2deaa8f1
denz1994 <denz1994@gmail.com>
2023-01-22 00:53:48 -0500

    Add endpoints for api. Specifically retrieving data for names, date ranges, and profession.

--- index.js
+++ index.js
@@ -6,14 +6,69 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Endpoints
 app.get('/', async (req, res) => {
     try {
-      const userData = await dbPool.query("SELECT * FROM UserInformationTable");
-      res.json(userData);
+      res.send("Connected Successfully")
     } catch (err) {
       console.error(err.message);
     }
   });
+
+app.get('/users', async (req, res) => {
+try {
+    const userData = await dbPool.query("SELECT * FROM UserInformationTable");
+    res.json(userData);
+} catch (err) {
+    console.error(err.message);
+}
+});
+
+app.get('/users/id/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userData = await dbPool.query("SELECT * FROM UserInformationTable WHERE id = ?",
+         [id]);
+        res.json(userData[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.get('/users/name/:firstName/:lastName', async (req, res) => {
+    try {
+        const { firstName, lastName } = req.params;
+        const userData = await dbPool.query("SELECT * FROM UserInformationTable WHERE firstName = ? AND lastName = ?",
+         [firstName, lastName]);
+        res.json(userData[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.get('/users/date/:start/:end', async (req, res) => {
+    try {
+        const { start, end } = req.params;
+        const startDate = new Date(start);
+        const endDate = new Date(end);
+        const userData = await dbPool.query("SELECT * FROM UserInformationTable WHERE ? <= dateCreated AND dateCreated <= ?",
+         [startDate, endDate]);
+        res.json(userData[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.get('/users/professions/:profession', async (req, res) => {
+    try {
+        const { profession } = req.params;
+        const userData = await dbPool.query("SELECT * FROM UserInformationTable WHERE profession = ?",
+         [profession]);
+        res.json(userData[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 const port = 3000;
 app.listen(port, () => {
